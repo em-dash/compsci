927f43b1dc0d2ce19a43400287d40158a18f0b00
Meredith Oleander <33614480+em-dash@users.noreply.github.com>
2021-11-05 14:45:50 +1100

    wfdlhawufulastulhaf

--- hash/hash.c
+++ hash/hash.c
@@ -6,7 +6,7 @@
 int main() {
     size_t i;
     FILE* file;
-    uint8_t* buffer;
+    uint8_t buffer[64];
     int c;
 
     file = fopen("test", "r");
@@ -14,12 +14,8 @@ int main() {
         fprintf(stderr, "can't open file\n");
         return 1;
     }
-    buffer = malloc(64 * sizeof uint8_t);
-    if (!buffer) {
-        fprintf(stderr, "failed to allocate memory\n");
-        return 1;
-    }
 
+    /* read 512 bits of the file, or until EOF */
     for (i = 0; i < 64; i++) {
         if ((c = fgetc(file)) != EOF) {
             buffer[i] = c;
@@ -28,11 +24,15 @@ int main() {
             break;
         }
     }
+    /* fill the rest of the 512 bit block if needed */
+    if (i < 64) {
+
+    for (; i < 64; i++) {
+        
 
     
     
     fclose(file);
-    free(buffer);
 
     return 0;
 }
