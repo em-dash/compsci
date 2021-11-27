463ea02b185a99c8e2bd9e479fecab68f78d52cd
em-dash <33614480+em-dash@users.noreply.github.com>
2021-11-27 11:28:18 +1100

    working on it

--- hash/hash.c
+++ hash/hash.c
@@ -1,16 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+
+/* left shift with rotate (why is this not in the standard library lol) */
+#define LROT32(n, x) (n << x) | (n >> 32 - x)
+
+/* F, G, H, I auxiliary function macros */
+#define AF(x, y, z) x & y | ~x & z
+#define AG(x, y, z) x & z | y & ~z
+#define AH(x, y, z) x ^ y ^ z
+#define AI(x, y, z) y ^ (x | ~z)
+
+
+/* a = b + ((a + F(b,c,d) + X[k] + T[i]) <<< s). */
+#define RF(a, b, c, d, xk, s, ti) a = b + (LROT32(a + AF(b, c, d) + xk + ti), s)
+
+/* a = b + ((a + G(b,c,d) + X[k] + T[i]) <<< s). */
+#define RG
+
+/* a = b + ((a + H(b,c,d) + X[k] + T[i]) <<< s). */
+#define RH
+
+/* a = b + ((a + I(b,c,d) + X[k] + T[i]) <<< s). */
+#define RI
+
 
 
 
 int main() {
-    size_t i;
     FILE* file;
     uint8_t* buffer;
-    int c;
+    uint32_t* buf_words;
     size_t buffer_len;
     long file_len;
+    uint64_t len_append;
+    uint64_t* len_append_addr;
+    /* words A B C D in order */
+    uint32_t mdbuf[4] = {0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476};
+    uint32_t mdbuf_prev[4];
+    uint32_t op_x[16];
+    size_t i;
+
+    /* TODO handle messages with a non-whole number of bytes */
+    /* TODO handle messages too big to fit in memory all at once */
+    /* TODO figure out portability to systems without uint8_t, and with other
+     * integer and type differences.  idk. */
 
     file = fopen("test", "r");
     if (!file) {
@@ -44,10 +79,128 @@ int main() {
     printf("%.*s\n", buffer_len, buffer);
     */
 
-    /* padding with bits, a single 1 followed by however many 0 */
-    
+    /* padding and appending length */
+    /* the actual message went up to buffer[file_len - 1] so we start padding at
+     * buffer[file_len] */
+    buffer[file_len] = (uint8_t) 128;
+    /* high index = buffer_len - 1 - 8
+     * low index = file_len + 1
+     * count = high index - low index + 1
+     * count = buffer_len - file_len - 9 */
+    /* TODO check integer conversions in buffer + file_len + 1 */
+    memset(buffer + file_len + 1, 0, buffer_len - file_len - 9);
+    /* TODO properly handle "the unlikely event that b is greater than 2^64" */
+    /* length of the original message in bits as a little-endian uint64_t */
+    /* TODO detect endianness lmao */
+#if defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+    /* TODO check integer conversions */
+    len_append_addr = (uint64_t*) (buffer + buffer_len - 8);
+    *len_append_addr = file_len * 8;
+#endif
+#if defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+#error "big endian not implemented (also not sure if macros work properly)"
+    /* TODO big endian */
+#endif
 
-    
+    /* process message */
+    /* number of blocks = buffer_len / 64 */
+    /* for each block i */
+    for (i = 0; i < buffer_len / 64; i++) {
+        /* copy block i into X (length is 64 bytes = 16 * 32-bit words) */
+        memcpy(op_x, i * 16, 64);
+        /* make a copy of 4 word buffer (mdbuf to mdbuf_prev */
+        memcpy(mdbuf_prev, mdbuf, 16);
+
+
+        /* intentionally allowing for overflows to wrap */
+
+        /* Round 1. */
+        /* Let [abcd k s i] denote the operation
+          a = b + ((a + F(b,c,d) + X[k] + T[i]) <<< s). */
+        /* Do the following 16 operations. */
+        [ABCD  0  7  1]
+        
+        [DABC  1 12  2]
+        [CDAB  2 17  3]
+        [BCDA  3 22  4]
+        [ABCD  4  7  5]
+        [DABC  5 12  6]
+        [CDAB  6 17  7]
+        [BCDA  7 22  8]
+        [ABCD  8  7  9]
+        [DABC  9 12 10]
+        [CDAB 10 17 11]
+        [BCDA 11 22 12]
+        [ABCD 12  7 13]
+        [DABC 13 12 14]
+        [CDAB 14 17 15]
+        [BCDA 15 22 16]
+
+        /* Round 2. */
+        /* Let [abcd k s i] denote the operation
+          a = b + ((a + G(b,c,d) + X[k] + T[i]) <<< s). */
+        /* Do the following 16 operations. */
+        [ABCD  1  5 17]
+        [DABC  6  9 18]
+        [CDAB 11 14 19]
+        [BCDA  0 20 20]
+        [ABCD  5  5 21]
+        [DABC 10  9 22]
+        [CDAB 15 14 23]
+        [BCDA  4 20 24]
+        [ABCD  9  5 25]
+        [DABC 14  9 26]
+        [CDAB  3 14 27]
+        [BCDA  8 20 28]
+        [ABCD 13  5 29]
+        [DABC  2  9 30]
+        [CDAB  7 14 31]
+        [BCDA 12 20 32]
+
+        /* Round 3. */
+        /* Let [abcd k s t] denote the operation
+          a = b + ((a + H(b,c,d) + X[k] + T[i]) <<< s). */
+        /* Do the following 16 operations. */
+        [ABCD  5  4 33]
+        [DABC  8 11 34]
+        [CDAB 11 16 35]
+        [BCDA 14 23 36]
+        [ABCD  1  4 37]
+        [DABC  4 11 38]
+        [CDAB  7 16 39]
+        [BCDA 10 23 40]
+        [ABCD 13  4 41]
+        [DABC  0 11 42]
+        [CDAB  3 16 43]
+        [BCDA  6 23 44]
+        [ABCD  9  4 45]
+        [DABC 12 11 46]
+        [CDAB 15 16 47]
+        [BCDA  2 23 48]
+
+        /* Round 4. */
+        /* Let [abcd k s t] denote the operation
+          a = b + ((a + I(b,c,d) + X[k] + T[i]) <<< s). */
+        /* Do the following 16 operations. */
+        [ABCD  0  6 49]
+        [DABC  7 10 50]
+        [CDAB 14 15 51]
+        [BCDA  5 21 52]
+        [ABCD 12  6 53]
+        [DABC  3 10 54]
+        [CDAB 10 15 55]
+        [BCDA  1 21 56]
+        [ABCD  8  6 57]
+        [DABC 15 10 58]
+        [CDAB  6 15 59]
+        [BCDA 13 21 60]
+        [ABCD  4  6 61]
+        [DABC 11 10 62]
+        [CDAB  2 15 63]
+        [BCDA  9 21 64]
+
+
+    }
     
     return 0;
 }
