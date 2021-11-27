56d75b5bfe99e94bc51fd2abde10b490bea3cc15
em-dash <33614480+em-dash@users.noreply.github.com>
2021-11-27 16:46:47 +1100

    good news!  it doesn't work

--- hash/hash.c
+++ hash/hash.c
@@ -3,8 +3,10 @@
 #include <stdint.h>
 #include <string.h>
 
+/* "word" means 32-bit unsigned integer */
+
 /* left shift with rotate (32-bit unsigned integers) */
-#define LROT32(n, x) (n << x) | (n >> 32 - x)
+#define LROT32(n, x) (n << x) | (n >> (32 - x))
 
 /* F, G, H, I auxiliary function macros */
 /* TODO check this, esp operator precedence */
@@ -16,19 +18,19 @@
 
 /* a = b + ((a + F(b,c,d) + X[k] + T[i]) <<< s). */
 #define RF(a, b, c, d, xk, s, ti) \
-    a = b + (LROT32(a + AF(b, c, d) + xk + ti), s)
+    a = b + (LROT32((a + AF(b, c, d) + xk + ti), s))
 
 /* a = b + ((a + G(b,c,d) + X[k] + T[i]) <<< s). */
 #define RG(a, b, c, d, xk, s, ti) \
-    a = b + (LROT32((a + AG(b, c, d) + xk + ti), s)
+    a = b + (LROT32((a + AG(b, c, d) + xk + ti), s))
 
 /* a = b + ((a + H(b,c,d) + X[k] + T[i]) <<< s). */
 #define RH(a, b, c, d, xk, s, ti) \
-    a = b + (LROT32((a + AH(b, c, d) + xk + ti), s)
+    a = b + (LROT32((a + AH(b, c, d) + xk + ti), s))
 
 /* a = b + ((a + I(b,c,d) + X[k] + T[i]) <<< s). */
 #define RI(a, b, c, d, xk, s, ti) \
-    a = b + (LROT32((a + AI(b, c, d) + xk + ti), s)
+    a = b + (LROT32((a + AI(b, c, d) + xk + ti), s))
 
 
 
@@ -131,88 +133,99 @@ int main() {
         /* Let [abcd k s i] denote the operation
           a = b + ((a + F(b,c,d) + X[k] + T[i]) <<< s). */
         /* Do the following 16 operations. */
-        RF(A, B, C, D,  op_x[0],  7, 0xd76aa478);
-        RF(D, A, B, C,  op_x[1], 12, 0xe8c7b756);
-        RF(C, D, A, B,  op_x[2], 17, 0x242070db);
-        RF(B, C, D, A,  op_x[3], 22, 0xc1bdceee);
-        RF(A, B, C, D,  op_x[4],  7, 0xf57c0faf);
-        RF(D, A, B, C,  op_x[5], 12, 0x4787c62a);
-        RF(C, D, A, B,  op_x[6], 17, 0xa8304613);
-        RF(B, C, D, A,  op_x[7], 22, 0xfd469501);
-        RF(A, B, C, D,  op_x[8],  7, 0x698098d8);
-        RF(D, A, B, C,  op_x[9], 12, 0x8b44f7af);
-        RF(C, D, A, B, op_x[10], 17, 0xffff5bb1);
-        RF(B, C, D, A, op_x[11], 22, 0x895cd7be);
-        RF(A, B, C, D, op_x[12],  7, 0x6b901122);
-        RF(D, A, B, C, op_x[13], 12, 0xfd987193);
-        RF(C, D, A, B, op_x[14], 17, 0xa679438e);
-        RF(B, C, D, A, op_x[15], 22, 0x49b40821);
+        RF(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],  op_x[0],  7, 0xd76aa478);
+        RF(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],  op_x[1], 12, 0xe8c7b756);
+        RF(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],  op_x[2], 17, 0x242070db);
+        RF(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],  op_x[3], 22, 0xc1bdceee);
+        RF(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],  op_x[4],  7, 0xf57c0faf);
+        RF(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],  op_x[5], 12, 0x4787c62a);
+        RF(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],  op_x[6], 17, 0xa8304613);
+        RF(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],  op_x[7], 22, 0xfd469501);
+        RF(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],  op_x[8],  7, 0x698098d8);
+        RF(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],  op_x[9], 12, 0x8b44f7af);
+        RF(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1], op_x[10], 17, 0xffff5bb1);
+        RF(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0], op_x[11], 22, 0x895cd7be);
+        RF(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3], op_x[12],  7, 0x6b901122);
+        RF(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2], op_x[13], 12, 0xfd987193);
+        RF(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1], op_x[14], 17, 0xa679438e);
+        RF(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0], op_x[15], 22, 0x49b40821);
+        /* glad these fit in 80 characters */
 
         /* Round 2. */
         /* Let [abcd k s i] denote the operation
           a = b + ((a + G(b,c,d) + X[k] + T[i]) <<< s). */
         /* Do the following 16 operations. */
-        RF(A, B, C, D,   op_x[1],  5, 0xf61e2562);
-        RF(D, A, B, C,   op_x[6],  9, 0xc040b340);
-        RF(C, D, A, B,  op_x[11], 14, 0x265e5a51);
-        RF(B, C, D, A,   op_x[0], 20, 0xe9b6c7aa);
-        RF(A, B, C, D,   op_x[5],  5, 0xd62f105d);
-        RF(D, A, B, C,  op_x[10],  9, 0x2441453);
-        RF(C, D, A, B,  op_x[15], 14, 0xd8a1e681);
-        RF(B, C, D, A,   op_x[4], 20, 0xe7d3fbc8);
-        RF(A, B, C, D,   op_x[9],  5, 0x21e1cde6);
-        RF(D, A, B, C,  op_x[14],  9, 0xc33707d6);
-        RF(C, D, A, B,   op_x[3], 14, 0xf4d50d87);
-        RF(B, C, D, A,   op_x[8], 20, 0x455a14ed);
-        RF(A, B, C, D,  op_x[13],  5, 0xa9e3e905);
-        RF(D, A, B, C,   op_x[2],  9, 0xfcefa3f8);
-        RF(C, D, A, B,   op_x[7], 14, 0x676f02d9);
-        RF(B, C, D, A,  op_x[12], 20, 0x8d2a4c8a);
+        RG(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],   op_x[1],  5, 0xf61e2562);
+        RG(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],   op_x[6],  9, 0xc040b340);
+        RG(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],  op_x[11], 14, 0x265e5a51);
+        RG(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],   op_x[0], 20, 0xe9b6c7aa);
+        RG(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],   op_x[5],  5, 0xd62f105d);
+        RG(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],  op_x[10],  9, 0x2441453);
+        RG(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],  op_x[15], 14, 0xd8a1e681);
+        RG(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],   op_x[4], 20, 0xe7d3fbc8);
+        RG(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],   op_x[9],  5, 0x21e1cde6);
+        RG(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],  op_x[14],  9, 0xc33707d6);
+        RG(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],   op_x[3], 14, 0xf4d50d87);
+        RG(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],   op_x[8], 20, 0x455a14ed);
+        RG(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],  op_x[13],  5, 0xa9e3e905);
+        RG(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],   op_x[2],  9, 0xfcefa3f8);
+        RG(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],   op_x[7], 14, 0x676f02d9);
+        RG(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],  op_x[12], 20, 0x8d2a4c8a);
 
         /* Round 3. */
         /* Let [abcd k s i] denote the operation
           a = b + ((a + H(b,c,d) + X[k] + T[i]) <<< s). */
         /* Do the following 16 operations. */
-        RF(A, B, C, D,   op_x[5],  4, 0xfffa3942);
-        RF(D, A, B, C,   op_x[8], 11, 0x8771f681);
-        RF(C, D, A, B,  op_x[11], 16, 0x6d9d6122);
-        RF(B, C, D, A,  op_x[14], 23, 0xfde5380c);
-        RF(A, B, C, D,   op_x[1],  4, 0xa4beea44);
-        RF(D, A, B, C,   op_x[4], 11, 0x4bdecfa9);
-        RF(C, D, A, B,   op_x[7], 16, 0xf6bb4b60);
-        RF(B, C, D, A,  op_x[10], 23, 0xbebfbc70);
-        RF(A, B, C, D,  op_x[13],  4, 0x289b7ec6);
-        RF(D, A, B, C,   op_x[0], 11, 0xeaa127fa);
-        RF(C, D, A, B,   op_x[3], 16, 0xd4ef3085);
-        RF(B, C, D, A,   op_x[6], 23, 0x4881d05);
-        RF(A, B, C, D,   op_x[9],  4, 0xd9d4d039);
-        RF(D, A, B, C,  op_x[12], 11, 0xe6db99e5);
-        RF(C, D, A, B,  op_x[15], 16, 0x1fa27cf8);
-        RF(B, C, D, A,   op_x[2], 23, 0xc4ac5665);
+        RH(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],   op_x[5],  4, 0xfffa3942);
+        RH(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],   op_x[8], 11, 0x8771f681);
+        RH(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],  op_x[11], 16, 0x6d9d6122);
+        RH(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],  op_x[14], 23, 0xfde5380c);
+        RH(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],   op_x[1],  4, 0xa4beea44);
+        RH(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],   op_x[4], 11, 0x4bdecfa9);
+        RH(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],   op_x[7], 16, 0xf6bb4b60);
+        RH(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],  op_x[10], 23, 0xbebfbc70);
+        RH(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],  op_x[13],  4, 0x289b7ec6);
+        RH(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],   op_x[0], 11, 0xeaa127fa);
+        RH(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],   op_x[3], 16, 0xd4ef3085);
+        RH(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],   op_x[6], 23, 0x4881d05);
+        RH(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],   op_x[9],  4, 0xd9d4d039);
+        RH(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],  op_x[12], 11, 0xe6db99e5);
+        RH(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],  op_x[15], 16, 0x1fa27cf8);
+        RH(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],   op_x[2], 23, 0xc4ac5665);
 
         /* Round 4. */
         /* Let [abcd k s i] denote the operation
           a = b + ((a + I(b,c,d) + X[k] + T[i]) <<< s). */
         /* Do the following 16 operations. */
-        RF(A, B, C, D,   op_x[0],  6, 0xf4292244);
-        RF(D, A, B, C,   op_x[7], 10, 0x432aff97);
-        RF(C, D, A, B,  op_x[14], 15, 0xab9423a7);
-        RF(B, C, D, A,   op_x[5], 21, 0xfc93a039);
-        RF(A, B, C, D,  op_x[12],  6, 0x655b59c3);
-        RF(D, A, B, C,   op_x[3], 10, 0x8f0ccc92);
-        RF(C, D, A, B,  op_x[10], 15, 0xffeff47d);
-        RF(B, C, D, A,   op_x[1], 21, 0x85845dd1);
-        RF(A, B, C, D,   op_x[8],  6, 0x6fa87e4f);
-        RF(D, A, B, C,  op_x[15], 10, 0xfe2ce6e0);
-        RF(C, D, A, B,   op_x[6], 15, 0xa3014314);
-        RF(B, C, D, A,  op_x[13], 21, 0x4e0811a1);
-        RF(A, B, C, D,   op_x[4],  6, 0xf7537e82);
-        RF(D, A, B, C,  op_x[11], 10, 0xbd3af235);
-        RF(C, D, A, B,   op_x[2], 15, 0x2ad7d2bb);
-        RF(B, C, D, A,   op_x[9], 21, 0xeb86d391);
-
-
+        RI(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],   op_x[0],  6, 0xf4292244);
+        RI(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],   op_x[7], 10, 0x432aff97);
+        RI(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],  op_x[14], 15, 0xab9423a7);
+        RI(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],   op_x[5], 21, 0xfc93a039);
+        RI(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],  op_x[12],  6, 0x655b59c3);
+        RI(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],   op_x[3], 10, 0x8f0ccc92);
+        RI(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],  op_x[10], 15, 0xffeff47d);
+        RI(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],   op_x[1], 21, 0x85845dd1);
+        RI(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],   op_x[8],  6, 0x6fa87e4f);
+        RI(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],  op_x[15], 10, 0xfe2ce6e0);
+        RI(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],   op_x[6], 15, 0xa3014314);
+        RI(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],  op_x[13], 21, 0x4e0811a1);
+        RI(mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3],   op_x[4],  6, 0xf7537e82);
+        RI(mdbuf[3], mdbuf[0], mdbuf[1], mdbuf[2],  op_x[11], 10, 0xbd3af235);
+        RI(mdbuf[2], mdbuf[3], mdbuf[0], mdbuf[1],   op_x[2], 15, 0x2ad7d2bb);
+        RI(mdbuf[1], mdbuf[2], mdbuf[3], mdbuf[0],   op_x[9], 21, 0xeb86d391);
     }
+
+#if defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+    printf("%.8x%.8x%.8x%.8x\n", mdbuf[0], mdbuf[1], mdbuf[2], mdbuf[3]);
+#endif
+
+
+#if defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+#error "big endian not implemented (also macros don't work properly)"
+    /* TODO big endian */
+#endif
+
+
     
     return 0;
 }
