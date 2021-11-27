4613e242b1c9dfbd94ddb719f57fda2c7fa95883
em-dash <33614480+em-dash@users.noreply.github.com>
2021-11-28 09:27:35 +1100

    you know what i'm trying rust first

--- hash/hash.zig
+++ hash/hash.zig
@@ -3,4 +3,9 @@ const std = @import("std");
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("hi\n", .{});
+
+    var file = try std.fs.cwd().openFile("foo.txt", .{});
+    defer file.close(); // TODO what is this lol
+
+    var buf_reader = io.bufferedReader
 }
