a1aa36be13c5545c13bfc339a1137b42effb0182
em-dash <33614480+em-dash@users.noreply.github.com>
2021-12-02 22:19:17 +1100

    cursed

--- util/types.c
+++ util/types.c
@@ -1,19 +1,27 @@
 #include "types.h"
 
-struct vec {
-    void * data;
-    size_t itemsz;
-    size_t len;
-    size_t maxlen;
-};
-
-/* if newsz is 0, size is rounded up to the next 2^n bytes, then doubled */
-struct vec * vappend(struct vec * const v, void * item) {
-
-    if (!newlen) {
-        while (i < v.len) {
-            i = i * 2;
-        }
-        return realloc(v, v.len = i * 2);
-    }
+#define VEC_TYPE(type) \
+struct vec_##type { \
+    type * data; \
+    size_t len; \
+    size_t maxlen; \
+}; \
+\
+struct vec_##type * vapnd_##type(struct vec_##type * const v, type item) { \
+    if (v.len < .maxlen) { \
+        v.data[len] = item; \
+        len++; \
+        return v; \
+    } \
+    vxtnd_##type(v); \
+    v.data[len] = item; \
+    return v; \
+} \
+\
+struct vec_##type * vxtnd_##type(struct vec_##type * const v) { \
+    size_t i = 2; \
+    while (i < v.len) { \
+        i = i * 2; \
+    } \
+    return realloc(v, v.len = i * 2); \
 }
